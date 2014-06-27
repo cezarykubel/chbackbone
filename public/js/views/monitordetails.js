3dd0ad305f334b4563c522ecffe999e79ec88b27
Cezary Kubel <fullredesigns@gmail.com>
2014-06-27 10:00:56 -0400

    Just a save

--- public/js/views/monitordetails.js
+++ public/js/views/monitordetails.js
@@ -58,62 +58,77 @@ window.DetailsView = Backbone.View.extend({
 
         /* ~~~~~~~~~~~~~ */
         // Sentiment Bar //
-        /* ~~~~~~~~~~~~~ *//*
+        /* ~~~~~~~~~~~~~ */
 
-            $('#charts').append("<div id='senBar'></div>");
-
-            $("#senBar").append("<p><strong>Sentiment Analysis</strong></p>");
-
+            
             var senPos = 0,
                 senNeu = 0,
                 senNeg = 0;
 
             var v1 = [];
+            var allCats = [];
 
             postCollection.at(0).attributes.categoryScores.forEach(function (entry){
                 v1.push(entry.categoryName);
             })
 
-            if(this.model.attributes.type == "Buzz") {
+            var senPosIndex = 0,
+                senNeuIndex = 0,
+                senNegIndex = 0,
+                hasBorG = false;
 
-                var senPosIndex = v1.indexOf("Basic Positive"),
-                    senNeuIndex = v1.indexOf("Basic Neutral"),
-                    senNegIndex = v1.indexOf("Basic Negative");
+            
+            senPosIndex = v1.indexOf("Basic Positive");
+            senNeuIndex = v1.indexOf("Basic Neutral");
+            senNegIndex = v1.indexOf("Basic Negative");
 
-            }
-            else if(this.model.attributes.type == "Opinion" || 
-                    this.model.attributes.type == "Social") {
-
-                var senPosIndex = v1.indexOf("General Positive", "Basic Positive"),
-                    senNeuIndex = v1.indexOf("General Neutral"),
-                    senNegIndex = v1.indexOf("General Negative");
-            }
-
-            for(var s = 0; s < len; s++) {
-
-                senPos += postCollection.at(z).attributes.categoryScores[senPosIndex].score;
-                senNeu += postCollection.at(z).attributes.categoryScores[senNeuIndex].score;
-                senNeg += postCollection.at(z).attributes.categoryScores[senNegIndex].score;
-
+            if(senPosIndex == -1) {
+                senPosIndex = v1.indexOf("General Positive");
+                senNeuIndex = v1.indexOf("General Neutral");
+                senNegIndex = v1.indexOf("General Negative");
             }
 
-            senPos /= len;
-            senNeu /= len;
-            senNeg /= len;
+            if(senPosIndex >= 0 || senNeuIndex >= 0 || senNegIndex >= 0){
+                hasBorG = true;
+            }
+            
+            console.log(v1);
+            console.log(hasBorG);
 
-            senPos = (senPos * 100).toFixed(1) + "%";
-            senNeu = (senNeu * 100).toFixed(1) + "%";
-            senNeg = (senNeg * 100).toFixed(1) + "%";
+            if(hasBorG) {
+
+                $('#charts').append("<div id='senBar'></div>");
+
+                $("#senBar").append("<p><strong>Sentiment Analysis</strong></p>");
+
+                for(var s = 0; s < len; s++) {
+
+                    senPos += postCollection.at(s).attributes.categoryScores[senPosIndex].score;
+                    senNeu += postCollection.at(s).attributes.categoryScores[senNeuIndex].score;
+                    senNeg += postCollection.at(s).attributes.categoryScores[senNegIndex].score;
+
+                }
+
+                console.log(len);
+                console.log(senPos);
+                console.log(senNeu);
+                console.log(senNeg);
+
+                senPos /= len;
+                senNeu /= len;
+                senNeg /= len;
+
+                senPos = ((senPos - 0.01) * 100).toFixed(1) + "%";
+                senNeu = ((senNeu - 0.01) * 100).toFixed(1) + "%";
+                senNeg = ((senNeg - 0.01) * 100).toFixed(1) + "%";
 
 
-            $('#senBar').append("<div class='senPos' style='width: "+senPos+"'></div>");
-            $('#senBar').append("<div class='senNeu' style='width: "+senNeu+"'></div>");
-            $('#senBar').append("<div class='senNeg' style='width: "+senNeg+"'></div>");
-            $('#senBar').append("<div class='clearfix'></div>");
+                $('#senBar').append("<div class='senPos' style='width: "+senPos+"'></div>");
+                $('#senBar').append("<div class='senNeu' style='width: "+senNeu+"'></div>");
+                $('#senBar').append("<div class='senNeg' style='width: "+senNeg+"'></div>");
+                $('#senBar').append("<div class='clearfix'></div>");
 
-    */
-
- 
+            }
 
         return this;
     }
